6acff2165107536e178eb08574b8a66c8a1d8543
Gaurav Chaurasia <gaurav.181cv155@nitk.edu.in>
2020-11-27 18:10:49 +0530

    add additional data on home page

--- controllers/home.js
+++ controllers/home.js
@@ -7,12 +7,37 @@
 // import local node modules
 // ----------------------------------------
 const { ROUTES } = require('../config/ROUTES');
+const Medicine = require('../models/medicine');
+const Disease = require('../models/disease');
 
-
-const getMain=(req, res) => {
-    res.render(
-        'home/main'
-    );
+const getMain = async (req, res) => {
+    try {
+        const topDisease = await Disease.find({}, { _id: 0 })
+                                        .sort({'updatedAt': -1})
+                                        .limit(5);
+        const topMedicine = await Medicine.find({}, { _id: 0 })
+                                          .sort({'updatedAt': -1})
+                                          .limit(5);
+        res.render(
+            'home/main', 
+            {
+                topDisease: topDisease,
+                topMedicine: topMedicine
+            }
+        );
+    } catch (err) {
+        req.flash(
+            'info', 
+            'Opps! Somthing went wrong, couldn\'t get all requied info!'
+        );
+        res.render(
+            'home/main', 
+            {
+                topDisease: null,
+                topMedicine: null
+            }
+        );
+    }
 };
 
 module.exports = {
