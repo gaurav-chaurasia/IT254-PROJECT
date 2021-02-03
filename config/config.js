706ec1dc57b42f373c20645b1349e51ceaab0e2d
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-02-03 23:30:31 +0530

    production readygit add .!

--- config/config.js
+++ config/config.js
@@ -8,5 +8,12 @@ module.exports = {
         'GOOGLE_OAUTH2_CLIENT_SECRET': process.env.GOOGLE_OAUTH2_CLIENT_SECRET,
     },
     "test": {},
-    "production": {}
+    "production": {
+        'DATABASE_URI': process.env.DATABASE_URI,
+        'secret': '12345-67890-09876-54321',
+        'session': 'IT254',
+        'GOOGLE_MAPS_APIKEY': process.env.GOOGLE_MAPS_APIKEY,
+        'GOOGLE_OAUTH2_CLIENT_ID': process.env.GOOGLE_OAUTH2_CLIENT_ID,
+        'GOOGLE_OAUTH2_CLIENT_SECRET': process.env.GOOGLE_OAUTH2_CLIENT_SECRET,
+    }
 }
