894cb288330837f8e4784c17ca31c91dc5181c1b
Upendra-123 <67254092+Upendra-123@users.noreply.github.com>
2021-10-06 15:47:51 +0530

    users updated

--- db/models/user.js
+++ db/models/user.js
@@ -28,4 +28,4 @@ const UserSchema = new Schema(
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema );
